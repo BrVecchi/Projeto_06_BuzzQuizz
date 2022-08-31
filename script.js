e5bceb9b79b4cab2efd067156da562857306fb69
Rayandson <rayandson.silva321@gmail.com>
2022-08-31 12:38:35 -0300

    Tela 3 (basic info) criada

---
+++ script.js
@@ -0,0 +1,12 @@
+let titulo = "";
+let imgURL = "";
+let qntdPerguntas = "";
+let qntdNiveis = "";
+
+
+function addInfo () {
+    titulo = document.querySelector(".titulo").value;
+    imgURL = document.querySelector(".img-url").value;
+    qntdPerguntas = document.querySelector(".qntd-perguntas").value;
+    qntdNiveis = document.querySelector(".qntd-niveis").value;
+}
