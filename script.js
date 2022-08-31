a741bbdba6762aca48b820f809f159368655617a
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-08-31 20:50:00 -0300

    salvando pra fazer o pull

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function addInfo () {
     qntdPerguntas = document.querySelector(".qntd-perguntas").value;
     qntdNiveis = document.querySelector(".qntd-niveis").value;
 }
+let botaosClicado;
+function selecionarquiz(botao){
+    console.log(botao);
+    const botaoNaoClicado = document.querySelectorAll('.selecionado');
+    console.log(botaoNaoClicado)
+   if(botaoNaoClicado.length === 0){
+   botao.classList.add('selecionado');
+   }
+}
